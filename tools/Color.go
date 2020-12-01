d0a64b3df39d977969b856793a82756ce9addc89
Civiled <civiled@usa.com>
2020-12-01 09:10:03 -0800

    Added texture

--- tools/Color.go
+++ tools/Color.go
@@ -4,3 +4,19 @@ type Color struct {
 	// TODO: Add reflection, roughness, and diffusion
 	R, G, B, A float32
 }
+
+type Texture struct {
+	R uint8 `json:"r"`
+
+	G uint8 `json:"g"`
+
+	B uint8 `json:"b"`
+
+	A uint8 `json:"a"`
+
+	Emission uint8 `json:"emission"`
+
+	Roughness uint8 `json:"roughness"`
+
+	Name string `json:"id"`
+}
