b4a75ee828895d48f6af425c7057e7fa503dac16
Civiled <civiled@usa.com>
2020-12-02 07:45:52 -0800

    Commented out and optimized ToArray()

--- tools/Vector.go
+++ tools/Vector.go
@@ -1,9 +1,14 @@
 package tools
-
+/**
+	Vector stores 3 float32 values to be used in 3D coordinate operations. These values are mapped to X, Y, Z
+ */
 type Vector struct {
 	X, Y, Z float32
 }
 
+/**
+	Equals checks if the current Vector and another vector are the same
+ */
 func (v *Vector) Equals(h *Vector) bool {
 	if h == nil {
 		return false
@@ -14,6 +19,9 @@ func (v *Vector) Equals(h *Vector) bool {
 	return false
 }
 
+/**
+	ToArray converts the float32 list starting with X and ending with Z
+ */
 func (v *Vector) ToArray() []float32 {
 	return []float32{v.X, v.Y, v.Z}
 }
