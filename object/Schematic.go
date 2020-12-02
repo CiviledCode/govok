3a71a4d6348d03d229a49a6a2a527dbd4e5b019d
Civiled <civiled@usa.com>
2020-12-02 07:49:35 -0800

    Comments and optimization to LoadColorMap

--- object/Schematic.go
+++ object/Schematic.go
@@ -7,21 +7,23 @@ import (
 	"io"
 )
 
+/**
+	LoadColorMap loads all of the texture data of a specific schematic using json.Unmarshal.
+	The name is always the namespace ID of the block we are mapping the texture to
+
+	file: The io.Reader pointing to the location of the file
+ */
 func LoadColorMap(file io.Reader) map[string]tools.Texture {
 	var cMap map[string]tools.Texture
 
+	// Open our scanner
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	var text []string
 
-	for scanner.Scan() {
-		text = append(text, scanner.Text())
-	}
-
+	// Go through all of the lines of the file and map the unmarshalled line to block ids in the map
 	var texture tools.Texture
-
-	for _, each_ln := range text {
-		json.Unmarshal([]byte(each_ln), texture)
+	for scanner.Scan() {
+		json.Unmarshal([]byte(scanner.Text()), texture)
 		cMap[texture.Name] = texture
 	}
 
