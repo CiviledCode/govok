ba3b10f2432ea19182973b1112be7f572e2a52bc
Civiled <civiled@usa.com>
2020-12-03 10:46:20 -0800

    Changed Vector Equal() params to use pointers

--- tools/Vector.go
+++ tools/Vector.go
@@ -1,12 +1,13 @@
 package tools
-/**
+
+/*
 	Vector stores 3 float32 values to be used in 3D coordinate operations. These values are mapped to X, Y, Z
  */
 type Vector struct {
 	X, Y, Z float32
 }
 
-/**
+/*
 	Equals checks if the current Vector and another vector are the same
  */
 func (v *Vector) Equals(h *Vector) bool {
@@ -19,7 +20,7 @@ func (v *Vector) Equals(h *Vector) bool {
 	return false
 }
 
-/**
+/*
 	ToArray converts the float32 list starting with X and ending with Z
  */
 func (v *Vector) ToArray() []float32 {
