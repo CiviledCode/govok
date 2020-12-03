2bacb5a01714f0fd2676b150c21a23194aae6cfa
Civiled <civiled@usa.com>
2020-12-03 14:45:30 -0800

    Recommenting and removed triangles as values due to plane system

--- shape/Plane.go
+++ shape/Plane.go
@@ -5,10 +5,11 @@ import (
 )
 
 /*
-	Plane is a 4 point shape made up of two triangles. We can default that all values are planes as there is no irregular shapes as everything is made of rectangles
+	Plane is a 4 point shape made up of 4 vectors. We can default to 4 points only because we are only handling regular rectangles, not irregular shapes with non-90 degree angles
  */
+
 type Plane struct {
-	TriangleOne, TriangleTwo *Triangle
+	X, Y, Z, W tools.Vector
 
 	Color *tools.Texture
 }
@@ -25,58 +26,21 @@ type Plane struct {
 func (p *Plane) ConvertToArray() []float32 {
 	array := make([]float32, 12)
 
-	values := toPointArray(p.TriangleOne, p.TriangleTwo)
+	array[0] = p.X.X
+	array[1] = p.X.Y
+	array[2] = p.X.Z
 
-	array[0] = values[0].X
-	array[1] = values[0].Y
-	array[2] = values[0].Z
+	array[3] = p.Y.X
+	array[4] = p.Y.Y
+	array[5] = p.Y.Z
 
-	array[3] = values[1].X
-	array[4] = values[1].Y
-	array[5] = values[1].Z
+	array[6] = p.Z.X
+	array[7] = p.Z.Y
+	array[8] = p.Z.Z
 
-	array[6] = values[2].X
-	array[7] = values[2].Y
-	array[8] = values[2].Z
-
-	array[9] = values[3].X
-	array[10] = values[3].Y
-	array[11] = values[3].Z
+	array[9] = p.W.X
+	array[10] = p.W.Y
+	array[11] = p.W.Z
 
 	return array
-}
-
-/*
-	toPointArray takes two triangles and finds the common and unique values and returns them in a list as follows: [t1 unique, common, t2 unique, common]
- */
-func toPointArray(t1 *Triangle, t2 *Triangle) []*tools.Vector {
-	vals := make([]*tools.Vector, 4)
-	var value *tools.Vector
-
-	// Find the common values between the triangles and the unique value of triangle one and add it to the list
-	for _, value = range t1.ToArray() {
-
-		if value.Equals(&t2.X) || value.Equals(&t2.Y) || value.Equals(&t2.Z) {
-			if vals[1] == nil {
-				vals[1] = value
-			} else {
-				vals[3] = value
-			}
-		} else {
-			if vals[0] == nil {
-				vals[0] = value
-			} else {
-				vals[2] = value
-			}
-		}
-	}
-
-	// Find the unique value of triangle two
-	for _, value = range t2.ToArray() {
-
-		if !value.Equals(&t1.X) || !value.Equals(&t1.Y) || !value.Equals(&t1.Z) {
-			vals[2] = value
-		}
-	}
-	return vals
 }
